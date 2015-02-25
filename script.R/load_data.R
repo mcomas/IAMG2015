d4a5109094815492aa403e4897ba4c7314477061
mcomas <marc@Marcs-Mac-mini.local>
2015-02-25 23:05:23 +0100

    S'agafeix una variable categorica al dataset.

--- script.R/load_data.R
+++ script.R/load_data.R
@@ -1,7 +1,6 @@
 library(rgdal)
 library(ggplot2)
 library(ggmap)
-library(spgrass6)
 
 data = read.csv('data/database_Antonella.csv', sep = ';')
 names(data)
@@ -16,19 +15,21 @@ names(coord) = c('x', 'y')
 coord$x = as.numeric(as.character(coord$x))
 coord$y = as.numeric(as.character(coord$y))
 
+d = with(data, cbind(coord, X, Facies))
+d = d[complete.cases(d),]
 
-X = na.omit(coord)
-coordinates(X) <- c('x','y')
-proj4string(X) <- CRS("+proj=tmerc +lat_0=0 +lon_0=9 +k=0.9996 +x_0=1500000 +y_0=0 +ellps=intl +units=m +no_defs")
+coordinates(d) <- c('x','y')
+proj4string(d) <- CRS("+proj=tmerc +lat_0=0 +lon_0=9 +k=0.9996 +x_0=1500000 +y_0=0 +ellps=intl +units=m +no_defs")
 
 CRS.new <- CRS("+init=epsg:4326")
-d.new <- spTransform(X, CRS.new)
+d <- data.frame(spTransform(d, CRS.new))
 
-ggplot(data.frame(d.new), aes(x=x, y=y)) + geom_point() + theme_bw()
+ggplot(data.frame(d), aes(x=x, y=y)) + geom_point() + theme_bw()
 
-D = data.frame(d.new)
-bbox <- ggmap::make_bbox(x, y, D, f = 0.5)
+bbox <- ggmap::make_bbox(x, y, d, f = 0.5)
 map = get_map(location = bbox, source="google", maptype='terrain', color='bw')
 
-ggmap(map) + geom_point(data=data.frame(d.new), aes(x = x, y = y), color='red', size=1,  alpha = .75)
-
+library(grid)
+ggmap(map) + geom_point(data=d, aes(x = x, y = y, color=substr(Facies, 1, 2)), size=4,  alpha = .75) +
+  xlab(NULL) + ylab(NULL)
+  
